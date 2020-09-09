5813c05602635a84052bd74c9a7d34f5de931a68
Matt Hudson <mrh4hd@mail.missouri.edu>
2020-09-09 18:59:34 -0500

    Start validation

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-register',
@@ -9,15 +10,26 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterPage implements OnInit {
 
   registerForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  usernameValidator(loginService: LoginService): ValidatorFn {
+    return ((control: AbstractControl): {[key: string]: any} | null => {
+      return (loginService.usernames.find((val) => val === control.value) == undefined) ? null : {
+        'usernameError': 'Username ' + control.value + ' is already taken!'
+      }
+    });
+  }
+  constructor(private formBuilder: FormBuilder, private loginService: LoginService) {
     this.registerForm = formBuilder.group({
       firstname: ['', Validators.required],
       lastname: ['', Validators.required],
       email: ['', [Validators.email, Validators.required]],
-      username: ['', Validators.required],
-      password: ['', Validators.required],
+      username: ['', [Validators.required, Validators.minLength(4), this.usernameValidator(loginService)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
       passwordConfirm: ['', Validators.required]
     });
+  }
+
+  isFormValid() {
+    return this.registerForm.valid && this.registerForm.value['password'] === this.registerForm.value['passwordConfirm'];
   }
 
   ngOnInit() {
